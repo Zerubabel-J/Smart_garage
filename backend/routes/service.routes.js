21643c9d02e6d327336ffa9ea96f889274c3ea27
Zerubabel <zerucodes@gmail.com>
2024-03-26 15:51:40 +0300

    services feature added from the backend

---
+++ backend/routes/service.routes.js
@@ -0,0 +1,22 @@
+// Import the express module
+const express = require("express");
+// Call the router method from express to create the router
+const router = express.Router();
+// Import the service controller
+const serviceController = require("../controllers/service.controller");
+// import authMiddleware
+const authMiddleware = require("../middlewares/auth.middleware");
+
+// Create a route to handle the add service request on post
+router.post("/api/service", serviceController.createService);
+// Create a route to handle the get all services request on get
+router.get("/api/services", serviceController.getAllServices);
+router.get("/api/service/:id", serviceController.getServiceById);
+
+// Create a route to handle deleting a service
+router.delete("/api/service/:id", serviceController.deleteService);
+
+// Create a route to handle editing a service
+router.patch("/api/service/:id", serviceController.editService);
+// Export the router
+module.exports = router;
